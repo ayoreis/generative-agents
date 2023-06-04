f9947b838c666a8b3fa966e17a0bc9bfb0252c07
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-06-04 14:32:01 +0100

    feat(memory): `__repr__` and `access` methods

--- memory.py
+++ memory.py
@@ -1,14 +1,23 @@
-import datetime
+from datetime import datetime
 
 from embeddings import embedding
 
-class Memory:
-    def __init__(self, natural_language_description: str, importance: int):
-        now = datetime.datetime.now()
 
-        self.natural_language_description = natural_language_description
+class Memory:
+    """4.1"""
+
+    def __init__(self, description: str, importance: int):
+        now = datetime.now()
+
+        self.description = description
         self.creation_timestamp = now
         self.most_recent_access_timestamp = now
 
         self.importance = importance
-        self.embedding = embedding(natural_language_description)
+        self.embedding = embedding(description)
+
+    def __repr__(self):
+        return self.description
+
+    def access(self):
+        self.most_recent_access_timestamp = datetime.now()
