a620b25284b8388ae444be25ecb49c5c711789b1
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-06-04 14:17:36 +0100

    refactor: Use generator comprehension

--- maths.py
+++ maths.py
@@ -8,14 +8,18 @@ MINUTES_IN_HOUR = 60
 
 SECONDS_IN_HOUR = MINUTES_IN_HOUR * SECONDS_IN_MINUTE
 
+
 def min_max_scale(x: Number, min: Number, max: Number):
     return (x - min) / (max - min)
 
+
 def dot_product(a: Vector, b: Vector):
-    return sum((i * j for i, j in zip(a, b)))
+    return sum(i * j for i, j in zip(a, b))
+
 
 def square_sum_sqrt(a: Vector):
-    return sqrt(sum((i ** 2 for i in a)))
+    return sqrt(sum(i**2 for i in a))
+
 
 def cosine_similarity(a: Vector, b: Vector):
     return dot_product(a, b) / (square_sum_sqrt(a) * square_sum_sqrt(b))
