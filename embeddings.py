f1ad12a7d0260efa09318f903815b9c484e0d67d
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-07-07 12:37:59 +0100

    style

--- embeddings.py
+++ embeddings.py
@@ -1,6 +1,7 @@
 from openai import Embedding
 
-MODEL = 'text-embedding-ada-002'
+MODEL = "text-embedding-ada-002"
+
 
 def embedding(text: str):
     return Embedding.create(model=MODEL, input=text).data[0].embedding
